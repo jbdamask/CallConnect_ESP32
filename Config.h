c74cab80e68c5701eb7da1e7e35dc0cc01b0dd04
johndamask <jbdamask@gmail.com>
2019-03-13 09:21:16 -0400

    Split certificates from other configuration params

--- Config.h
+++ Config.h
@@ -7,22 +7,3 @@ const char *subscribeTopic = "<topic>"; // Can be changed to shadow topics...
 const char *publishTopic = "<topic>";
 #define NUMPIXELS1      16 // number of LEDs 
 
-
-// Paste the file contents into the certificate_pem_crt array in the following fashion. Make sure you follow these four rules when doing so:
-
-// Do not add any extra whitespaces
-// End your lines with: \n\
-// End the last line with: \n
-// Put the array in quotes: ""
-
-const char rootCABuff[] = {"-----BEGIN CERTIFICATE-----\n\
-example\n\
------END CERTIFICATE-----\n"};
-
-const char certificateBuff[] = {"-----BEGIN CERTIFICATE-----\n\
-example\n\
------END CERTIFICATE-----\n"};
-
-const char privateKeyBuff[] = {"-----BEGIN CERTIFICATE-----\n\
-example\n\
------END CERTIFICATE-----\n"};

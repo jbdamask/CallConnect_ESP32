f4dcc56e7ce6a23e1be784e8a70365250385fdb3
johndamask <jbdamask@gmail.com>
2019-03-18 12:32:46 -0400

    Added access point password param

--- Config.h
+++ Config.h
@@ -6,6 +6,7 @@ const char *awsEndPoint = "<iotendpoint>";
 const char *subscribeTopic = "<topic>"; // Can be changed to shadow topics...
 const char *publishTopic = "<topic>";
 const long RESET_AFTER = 21600000;  // 6 hours
+const char *AP_PASSWORD = "<access point password>";
 const long LONG_PRESS_DURATION = 5000; // Used for long button push to restart the ESP chip
 #define NUMPIXELS1      16 // number of LEDs 
 
